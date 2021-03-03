93196b6502b87a4d0fee8d718449ff765810d62b
vd11-unsw <61264311+vd11-unsw@users.noreply.github.com>
2021-03-04 02:28:57 +1100

    V0.1d2 - Visuals for splash implemented - Programming commenced

--- app/src/main/java/com/example/custodian/MainActivity.java
+++ app/src/main/java/com/example/custodian/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.custodian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.NOT_FOCUSABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+    }
+
+    private ImageButton mLoginButton;
+    private ImageButton mInformationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginButton = findViewById(R.id.ibMainLogin);
+        mInformationButton = findViewById(R.id.ibMainInformation);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Login Button Clicked");
+            }
+        });
+
+        mInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Information Button Clicked");
+            }
+        });
     }
 }
