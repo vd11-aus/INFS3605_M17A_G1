b22e4c0f760a38b92a5c129ebe48d25db331bb04
vd11-unsw <61264311+vd11-unsw@users.noreply.github.com>
2021-03-12 03:13:58 +1100

    V0.2 - Programming continued - Basic login and create account system implemented - Room for more user fields? - Edit account details is a possibility

--- app/src/main/java/com/example/custodian/CreateAccountActivity.java
+++ app/src/main/java/com/example/custodian/CreateAccountActivity.java
@@ -1,12 +1,31 @@
 package com.example.custodian;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -16,8 +35,22 @@ public class CreateAccountActivity extends AppCompatActivity {
     }
 
     // Declaration of variables
+    private EditText mUsernameInput;
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
+    private EditText mConfirmPasswordInput;
+    private RadioGroup mOriginInput;
     private ImageButton mCreateAccountButton;
     private ImageButton mCancelButton;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseDB;
+
+    String username = "";
+    String email = "";
+    String password = "";
+    String passwordConfirm = "";
+    String origin = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +60,59 @@ public class CreateAccountActivity extends AppCompatActivity {
         // Assigning of variable values
         mCreateAccountButton = findViewById(R.id.ibCreateAccountContinue);
         mCancelButton = findViewById(R.id.ibCreateAccountCancel);
+        mUsernameInput = findViewById(R.id.etCreateAccountUsername);
+        mEmailInput = findViewById(R.id.etCreateAccountEmail);
+        mPasswordInput = findViewById(R.id.etCreateAccountPassword);
+        mConfirmPasswordInput = findViewById(R.id.etCreateAccountConfirmPassword);
+        mOriginInput = findViewById(R.id.rgCreateAccountOrigin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDB = FirebaseFirestore.getInstance();
 
         // Confirm create account
         mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Create Account Button Clicked");
+                switch (entriesComplete()) {
+                    case "incomplete-entry":
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "One or more text fields are blank, please fill them and try again.", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case "invalid-username":
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "Invalid username, it has to be a minimum of 8 characters with no spaces.", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case "invalid-email":
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "Invalid email, it has to contain the '@' symbol.", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case "invalid-password":
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "Invalid password, it has to be a minimum of 8 characters with no spaces.", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case "passwords-dont-match":
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "The passwords you've provided don't match each other. Please correct them and try again.", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case "entries-validated":
+                        originQuestionCheck();
+                        System.out.println("Username: " + username);
+                        System.out.println("Email: " + email);
+                        System.out.println("Password: " + password);
+                        System.out.println("Origin: " + origin);
+                        Snackbar.make(findViewById(R.id.clCreateAccountMainLayout),
+                                "Creating user ...", Snackbar.LENGTH_SHORT).show();
+                        createUser();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                addUserDetails();
+                                launchWelcomeActivity();
+                            }
+                        }, 2500);
+                        break;
+                }
             }
         });
 
@@ -41,9 +121,96 @@ public class CreateAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 System.out.println("Cancel Button Clicked");
+                showWarningDialog(getCurrentFocus());
+            }
+        });
+    }
+
+    // Check origin entry
+    private void originQuestionCheck() {
+        switch (mOriginInput.getCheckedRadioButtonId()) {
+            case R.id.rbCreateAccountOriginTrue:
+                origin = "True";
+                break;
+            case R.id.rbCreateAccountOriginFalse:
+                origin = "False";
+                break;
+            case R.id.rbCreateAccountOriginDoNotDisclose:
+                origin = "Did Not Disclose";
+                break;
+        }
+    }
+
+    // Check form entries
+    private String entriesComplete() {
+        String returnValue = "loading ...";
+        username = mUsernameInput.getText().toString();
+        email = mEmailInput.getText().toString();
+        password = mPasswordInput.getText().toString();
+        passwordConfirm = mConfirmPasswordInput.getText().toString();
+        if (username.equals("") || email.equals("") || password.equals("") || passwordConfirm.equals("")) {
+            returnValue = "incomplete-entry";
+        } else if (username.length() < 8 || username.contains(" ")) {
+            returnValue = "invalid-username";
+        } else if (!email.contains("@")) {
+            returnValue = "invalid-email";
+        } else if (password.length() < 8 || password.contains(" ")) {
+            returnValue = "invalid-password";
+        } else if (!passwordConfirm.equals(password)) {
+            returnValue = "passwords-dont-match";
+        } else {
+            returnValue = "entries-validated";
+        }
+        System.out.println("Validation Outcome: " + returnValue);
+        return returnValue;
+    }
+
+    // Adds user in Firebase Authentication
+    public void createUser() {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+            }
+        });
+    }
+
+    // Adds user document in Firebase Cloud Firestore
+    public void addUserDetails() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", username);
+        map.put("origin", origin);
+        firebaseDB.collection("users").document(firebaseAuth.getCurrentUser().getUid()).set(map)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                    }
+                });
+    }
+
+    // Warn user that any entered data will be wiped
+    public void showWarningDialog(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Are you sure?");
+        alert.setMessage("If you quit, any form entries you have made thus far will be deleted. If you decide against this later, " +
+                "you will have to re-enter your details.");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
                 launchLoginActivity();
             }
         });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.create().show();
+    }
+
+    // Go to Welcome Activity
+    private void launchWelcomeActivity() {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
     }
 
     // Go to LoginActivity
--- app/src/main/java/com/example/custodian/WelcomeActivity.java
+++ app/src/main/java/com/example/custodian/WelcomeActivity.java
@@ -1,8 +1,27 @@
 package com.example.custodian;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Document;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -11,9 +30,51 @@ public class WelcomeActivity extends AppCompatActivity {
     public void onBackPressed() {
     }
 
+    // Declaration of variables
+    private TextView mUsernameText;
+    private Button mNextPageButton;
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseFirestore mFirebaseDB;
+    String username;
+    Integer origin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // Assigning of variable values
+        mUsernameText = findViewById(R.id.tvWelcomeUsername);
+        mNextPageButton = findViewById(R.id.btWelcomeNextPage);
+        mNextPageButton.setEnabled(false);
+
+        // Verify user found
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            System.out.println(currentUser.getUid());
+        }
+
+        // Connect to Firestore DB
+        String uniqueIdentifier = currentUser.getUid();
+        mFirebaseDB = FirebaseFirestore.getInstance();
+        DocumentReference referencePathOne = mFirebaseDB.document("users/"+uniqueIdentifier);
+        referencePathOne.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                username = value.getString("username");
+                mUsernameText.setText(username);
+                mNextPageButton.setEnabled(true);
+            }
+        });
+
+        // Navigate to user home
+        mNextPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Next page button clicked.");
+            }
+        });
     }
 }
