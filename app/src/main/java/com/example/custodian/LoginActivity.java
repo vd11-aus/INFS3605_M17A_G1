320510f34abb8d68d1660c8c2fa6ff94ab7d0a6f
vd11-unsw <61264311+vd11-unsw@users.noreply.github.com>
2021-03-12 00:01:39 +1100

    V0.1f1 - Programming continued - Login page complete

--- app/src/main/java/com/example/custodian/LoginActivity.java
+++ app/src/main/java/com/example/custodian/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.custodian;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -9,11 +10,16 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +35,10 @@ public class LoginActivity extends AppCompatActivity {
     private Button mLoginButton;
     private Button mCreateAccountButton;
     private TextView mForgotDetailsButton;
+    private EditText mEmailText;
+    private EditText mPasswordText;
+
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,9 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton = findViewById(R.id.btLoginLogin);
         mCreateAccountButton = findViewById(R.id.btLoginCreateAccount);
         mForgotDetailsButton = findViewById(R.id.tvLoginForgotDetails);
+        mEmailText = findViewById(R.id.etLoginEmailInput);
+        mPasswordText = findViewById(R.id.etLoginPasswordInput);
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         // Cancel log in
         mExitButton.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +70,10 @@ public class LoginActivity extends AppCompatActivity {
                 "This is where you enter the email you used to create this account.", Snackbar.LENGTH_SHORT);
         Snackbar passwordHelpMessage = Snackbar.make(findViewById(R.id.clLoginMainLayout),
                 "This is where you enter the password associated with the email you used to create this account.", Snackbar.LENGTH_SHORT);
+        Snackbar wrongCredentialsMessage = Snackbar.make(findViewById(R.id.clLoginMainLayout),
+                "There's something wrong with the email or password that you entered. Please try again.", Snackbar.LENGTH_SHORT);
+        Snackbar invalidCredentialsMessage = Snackbar.make(findViewById(R.id.clLoginMainLayout),
+                "Please enter an email and password.", Snackbar.LENGTH_SHORT);
 
         // Request for help on email input
         mEmailHelpButton.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +98,23 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 System.out.println("Log In Button Clicked");
+                String email = mEmailText.getText().toString();
+                String password = mPasswordText.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    invalidCredentialsMessage.show();
+                } else {
+                    mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                launchWelcomeActivity();
+                            } else {
+                                wrongCredentialsMessage.show();
+                            }
+                        }
+                    });
+                }
             }
         });
 
@@ -119,6 +153,12 @@ public class LoginActivity extends AppCompatActivity {
     // Go to Account Recovery Website
     private void launchAccountRecoveryWebsite() {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.custodian.com.au/forgot-my-credentials/"));
+        startActivity(intent);
+    }
+
+    // Go to Welcome Activity
+    private void launchWelcomeActivity() {
+        Intent intent = new Intent(this, WelcomeActivity.class);
         startActivity(intent);
     }
 }
